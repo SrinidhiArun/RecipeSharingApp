ac4b00442629fb7df31ea313a857382bc5fab8f1
Srinidhi <srinidhi>
2021-03-17 23:38:43 -0500

    Hello world project setup

---
+++ src/main/java/org/launchcode/recipeapp/controllers/HelloController.java
@@ -0,0 +1,16 @@
+package org.launchcode.recipeapp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+
+    @RequestMapping(value= "hello")
+    @ResponseBody
+    public String sayHello(){
+        return "Hello World!!";
+    }
+}
